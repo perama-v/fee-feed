d272788158889a785e7918d513bd69103d13d7de
dil <contact@martinstuessy.com>
2021-08-01 11:34:57 +0100

    semicolon

--- fee-feed.py
+++ fee-feed.py
@@ -12,7 +12,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Change this to the address of your node.
-node = os.environ.get('RPC_URL');
+node = os.environ.get('RPC_URL')
 
 ModeNames = Enum('Modes', 'LATEST_FEES BASE_FEE FULLNESS SPREAD BURN')
 mode_keys = [ord('1'), ord('2'), ord('3'), ord('4'), ord('5')]
